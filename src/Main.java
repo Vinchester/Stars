88c23fb72961f0d32495626c4ad21d17abf1aa4b
DIT Happy <m.h.kuznetsov@student.khai.edu>
2023-04-03 16:54:41 +0300

    Frame added

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public class Main {
+    public static void main(String[] args) {
+        final int screenWidth = 1200;
+        final int screenHeight = 800;
+
+        JFrame frame = new JFrame();
+        frame.setSize(screenWidth, screenHeight);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        BufferedImage image = new BufferedImage(screenWidth, screenHeight, BufferedImage.TYPE_INT_ARGB);
+
+        ImageIcon imageIcon = new ImageIcon(image);
+
+        JLabel picLabel = new JLabel(imageIcon);
+        BorderLayout borderLayout = new BorderLayout();
+        frame.getContentPane().setLayout(borderLayout);
+        frame.getContentPane().add(picLabel, BorderLayout.CENTER);
+
+        frame.setVisible(true);
+
+    }
+}
